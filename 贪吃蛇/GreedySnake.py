19965a81a20d14ca1c93f3846eac11a28236a1ef
hwm-1114 <1013052755@qq.com>
2023-08-20 10:39:40 +0800

    绘制蛇头，食物和蛇身的位置

--- 贪吃蛇/GreedySnake.py
+++ 贪吃蛇/GreedySnake.py
@@ -1,5 +1,5 @@
-# 1. 搭建初始框架，绘制场景
 '''
+1. 搭建初始框架，绘制场景
 第一步就是搭建一个初始界面，包括
 设置界面的size
 设置关闭界面事件
@@ -7,9 +7,8 @@
 页面背景渲染
 更新页面
 '''
-
-##-------------------------------------------------------
 import pygame
+import random
 
 # 游戏初始化
 pygame.init()
@@ -27,3 +26,22 @@ green = (0, 255, 0)
 # 设置游戏窗口
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('贪吃蛇')
+
+'''
+2.绘制蛇头，食物和蛇身的位置
+'''
+# 蛇头坐标
+snake_x = window_width / 2
+snake_y = window_height / 2
+
+# 蛇身列表
+snake_body = [[snake_x, snake_y]]
+
+# 食物坐标
+food_x = round(random.randrange(0, window_width - 20) / 20) * 20
+food_y = round(random.randrange(0, window_height - 20) / 20) * 20
+
+
+
+
+
